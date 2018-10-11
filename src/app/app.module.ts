5bbe2648b98a874095a3a2fe53669191e45389e2
Vishesh <visheshsingh16@gmail.com>
2018-10-11 16:14:40 -0400

    Data down from parent to child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OuterComponent } from './outer/outer.component';
+import { InnerComponent } from './inner/inner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OuterComponent,
+    InnerComponent
   ],
   imports: [
     BrowserModule
